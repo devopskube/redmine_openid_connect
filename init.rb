c0bb25533ae88360edc2a1ea973b3140ecd422d5
madumlao <madumlao@gmail.com>
2016-01-19 19:53:44 +0800

    Update init.rb with credits to ajdillera

--- init.rb
+++ init.rb
@@ -7,9 +7,9 @@ Redmine::Plugin.register :redmine_openid_connect do
   name 'Redmine Openid Connect plugin'
   author 'Alfonso Juan Dillera'
   description 'OpenID Connect implementation for Redmine'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  version '0.0.2'
+  url 'http://bitbucket.org/intelimina/redmine_openid_connect'
+  author_url 'http://github.com/adillera'
 
   settings :default => { 'empty' => true }, partial: 'settings/redmine_openid_connect_settings'
 end
