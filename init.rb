89d1be324696aba2ebcbc3465cb605a292467577
madumlao <madumlao@gmail.com>
2016-02-08 23:56:17 +0800

    Version bump to 0.9

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_openid_connect do
   name 'Redmine Openid Connect plugin'
   author 'Alfonso Juan Dillera'
   description 'OpenID Connect implementation for Redmine'
-  version '0.0.2'
+  version '0.9'
   url 'http://bitbucket.org/intelimina/redmine_openid_connect'
   author_url 'http://github.com/adillera'
 
