42dd7442fea30b846fb75bb27f19b65d05c50970
madumlao <madumlao@gmail.com>
2016-02-09 18:19:23 +0800

    Separate the index from the column definition for rails 3 compat

--- db/migrate/001_create_oic_sessions.rb
+++ db/migrate/001_create_oic_sessions.rb
@@ -1,18 +1,21 @@
 class CreateOicSessions < ActiveRecord::Migration
   def self.up
     create_table :oic_sessions do |t|
-      t.references :user, index: true, foreign_key: true
+      t.references :user, foreign_key: true
       t.string :code
       t.string :state
       t.string :nonce
       t.string :session_state
       t.text :id_token
-      t.string :access_token, index: true
-      t.string :refresh_token, index: true
+      t.string :access_token
+      t.string :refresh_token
       t.datetime :expires_at
       t.timestamps
     end
 
+    add_index :oic_sessions, :user_id
+    add_index :oic_sessions, :access_token
+    add_index :oic_sessions, :refresh_token
     add_index :oic_sessions, :id_token, length: 64
   end
   def self.down
