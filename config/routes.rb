cd57dee408bcb3eb5d1935c88f5017cd8a7718bb
madumlao <madumlao@gmail.com>
2016-03-01 08:34:07 +0800

    Add namespaced routes rooted in /oic

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@
 
 get 'oic', to: 'account#oic'
 get 'oic_reauthorize', to: 'account#oic_reauthorize'
-get 'rpiframe', to: 'account#rpiframe'
+get 'oic/login', to: 'account#oic_login'
+get 'oic/logout', to: 'account#oic_logout'
+get 'oic/local_login', to: 'account#oic_local_login'
+get 'oic/local_logout', to: 'account#oic_local_logout'
+get 'oic/rpiframe', to: 'account#rpiframe'
