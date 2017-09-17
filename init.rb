2150ad281f054aa6416aea3fd527da839d08ac87
Markus M. May <triplem@javafreedom.org>
2017-09-17 15:18:27 +0200

    Try to fix settings problem

--- init.rb
+++ init.rb
@@ -5,10 +5,10 @@ require 'redmine_openid_connect/hooks'
 
 Redmine::Plugin.register :redmine_openid_connect do
   name 'Redmine Openid Connect plugin'
-  author 'Alfonso Juan Dillera'
+  author 'Alfonso Juan Dillera / Markus M. May'
   description 'OpenID Connect implementation for Redmine'
   version '0.9.2'
-  url 'http://bitbucket.org/intelimina/redmine_openid_connect'
+  url 'https://github.com/devopskube/redmine_git_remote'
   author_url 'http://github.com/adillera'
 
   settings :default => { 'empty' => true }, partial: 'settings/redmine_openid_connect_settings'
