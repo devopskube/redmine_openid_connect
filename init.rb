bd46e6031f62190c5845029b7fc3e9d305d01f73
ben <uaqben@disroot.org>
2018-03-21 19:51:42 +0100

    fixed git repo URL; it was incorrect

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_openid_connect do
   author 'Alfonso Juan Dillera / Markus M. May'
   description 'OpenID Connect implementation for Redmine'
   version '0.9.3'
-  url 'https://github.com/devopskube/redmine_git_remote'
+  url 'https://github.com/devopskube/redmine_openid_connect'
   author_url 'http://github.com/adillera'
 
   settings :default => { 'empty' => true }, partial: 'settings/redmine_openid_connect_settings'
