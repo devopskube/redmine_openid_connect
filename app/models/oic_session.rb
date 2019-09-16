0801b72acdcf45cbee01dc57c202502089cb8d06
Сапоненко Денис <dsaponenko@systematica-consulting.ru>
2019-09-16 13:02:22 +0300

    jwt token decoding fix by https://auth0.com/docs/tokens/reference/jwt/jwt-structure and https://www.rfc-editor.org/rfc/rfc7519.txt paragraph 3

--- app/models/oic_session.rb
+++ app/models/oic_session.rb
@@ -86,7 +86,7 @@ class OicSession < ActiveRecord::Base
 
   def self.parse_token(token)
     jwt = token.split('.')
-    return JSON::parse(Base64::decode64(jwt[1]))
+    return JSON::parse(Base64::urlsafe_decode64(jwt[1]))
   end
 
   def claims
@@ -143,7 +143,7 @@ class OicSession < ActiveRecord::Base
 
   def user
     if @user.blank? || id_token_changed?
-      @user = JSON::parse(Base64::decode64(id_token.split('.')[1]))
+      @user = JSON::parse(Base64::urlsafe_decode64(id_token.split('.')[1]))
     end
     return @user
   end
