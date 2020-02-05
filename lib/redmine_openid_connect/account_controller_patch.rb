098fe3c3b71cd2c7aa48edbbf813952b79285396
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2020-02-05 22:03:43 +0300

    convert mail to login add user groups at login

--- lib/redmine_openid_connect/account_controller_patch.rb
+++ lib/redmine_openid_connect/account_controller_patch.rb
@@ -212,20 +212,10 @@ module RedmineOpenidConnect
       groups.each do |group|
         begin
           rm_g = Group.find_by(:lastname => group.to_s.downcase)
-          if rm_g
-            logger.error("group found")
-            if not rm_g.users.include?(user)
-              rm_g.users << user
-              logger.error("user added to group")
-            end
-          else
-            logger.error("no group " + group)
-            g = Group.new()
-            g.name = group
-            g.user_ids = [user.id]
-            if g.save()
-              logger.error("group added")
-            end
+          logger.error("group found")
+          if not rm_g.user_ids.include? user.id
+            rm_g.users << user
+            logger.error("user added to group")
           end
         rescue ActiveRecord::RecordNotFound
           logger.error("no group " + group)
