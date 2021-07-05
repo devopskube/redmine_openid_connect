6d4b6b45e710046b59453a54461bfdb2baddd96a
galupa <rangga.w.a.p@gmail.com>
2021-07-06 10:07:59 +1200

    patch delete users

--- init.rb
+++ init.rb
@@ -1,7 +1,8 @@
 require 'redmine'
-require_relative 'lib/redmine_openid_connect/application_controller_patch'
-require_relative 'lib/redmine_openid_connect/account_controller_patch'
-require_relative 'lib/redmine_openid_connect/hooks'
+require 'lib/redmine_openid_connect/application_controller_patch'
+require 'lib/redmine_openid_connect/account_controller_patch'
+require 'lib/redmine_openid_connect/users_controller_patch'
+require 'lib/redmine_openid_connect/hooks'
 
 Redmine::Plugin.register :redmine_openid_connect do
   name 'Redmine Openid Connect plugin'
@@ -14,7 +15,8 @@ Redmine::Plugin.register :redmine_openid_connect do
   settings :default => { 'empty' => true }, partial: 'settings/redmine_openid_connect_settings'
 end
 
-
-ApplicationController.prepend(RedmineOpenidConnect::ApplicationControllerPatch)
-AccountController.prepend(RedmineOpenidConnect::AccountControllerPatch)
-
+Rails.configuration.to_prepare do
+  ApplicationController.prepend(RedmineOpenidConnect::ApplicationControllerPatch)
+  AccountController.prepend(RedmineOpenidConnect::AccountControllerPatch)
+  UsersController.prepend(RedmineOpenidConnect::UsersControllerPatch)
+end
