e047a53c4000208b69de9d03994e19a54e83b2c6
Inti Tidball <inti.tidball@mikroways.net>
2023-05-08 11:46:20 -0300

    Fix, require relative en vez de require

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 require 'redmine'
-require 'lib/redmine_openid_connect/application_controller_patch'
-require 'lib/redmine_openid_connect/account_controller_patch'
-require 'lib/redmine_openid_connect/users_controller_patch'
-require 'lib/redmine_openid_connect/hooks'
+require_relative 'lib/redmine_openid_connect/application_controller_patch'
+require_relative 'lib/redmine_openid_connect/account_controller_patch'
+require_relative 'lib/redmine_openid_connect/users_controller_patch'
+require_relative 'lib/redmine_openid_connect/hooks'
 
 Redmine::Plugin.register :redmine_openid_connect do
   name 'Redmine Openid Connect plugin'
